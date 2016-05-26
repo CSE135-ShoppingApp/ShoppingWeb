367433892d1d40347640acd50300bec0ba329bb7
Jose A. Martinez <jam004@ucsd.edu>
2016-05-26 09:39:54 -0700

    Latest changes. Last version.

--- Shoppa/Shoppa/Controllers/AnalyticsController.cs
+++ Shoppa/Shoppa/Controllers/AnalyticsController.cs
@@ -15,146 +15,157 @@ namespace Shoppa.Controllers
         shoppa135dbEntities context = new shoppa135dbEntities();
 
         // GET: Analytics
-        public ActionResult Sales(string rowsMenu, string orderMenu, string productCategories, int rowPage = 0, int columnPage = 0)
+        public ActionResult Sales()
         {
             // FILTERING OPTIONS:
-            string[] rowsMenuList = new string[] { "Customers", "States" };
+            var rowsMenuList =
+                new List<KeyValuePair<string, string>>()
+                    { new KeyValuePair<string,string>("States", "state"), 
+                      new KeyValuePair<string,string>("Customers", "name")};
+
+            var orderMenuList =
+                new List<KeyValuePair<string, string>>()
+                    { new KeyValuePair<string,string>("Alphabetical", "alphabet"), 
+                      new KeyValuePair<string,string>("Top-K", "topk")};
+
+            // Get Categories
+            var categories = context.categories.ToList();
+
+            int currentPage = 0;
+            int currentColumn = 0;
+            string rowsMenu = "name";
+            string orderMenu = "alphabet";
+            int productCategories = 0;
+
+            string rowsCriteria = "Customers";
+
+            int startRow = 0;
+            int startColumn = 0;
+            bool hideNextCol = false;
+            bool hideNextPage = false;
+
+            int maxCols = context.products.Count();
+
+            if (startColumn + 9 >= maxCols)
+            {
+                hideNextCol = true;
+            }
+
+            int maxRows = context.users.Count();
+     
+            if (startRow + 19 >= maxRows)
+            {
+                hideNextPage = true;
+            }
+
             // Select default and assign list
-            ViewBag.RowsMenu = new SelectList(rowsMenuList, rowsMenu ?? "Customers");
-            string[] orderMenuList = new string[] { "Alphabetical", "Top-K" };
-            ViewBag.OrderMenu = new SelectList(orderMenuList, orderMenu ?? "Alphabetical");
-            var productCategoriesList = context.categories.Select(x => x.name).ToList();
-            ViewBag.ProductCategories = new SelectList(productCategoriesList, productCategories ?? "All");
-
-            string rows = "name";
-            if (rowsMenu == "States")
-            {
-                rows = "state";
-            }
-
-            string order = "alphabet";
-            if (orderMenu == "Top-K")
-            {
-                order = "topk";
-            }
-
-            int startRow = (rowPage * 20) + 1;
-            int startColumn = (columnPage * 10) + 1;
-
-            ViewBag.RowPage = rowPage;
-            ViewBag.ColumnPage = columnPage;
-
+            ViewBag.RowsMenu = new SelectList(rowsMenuList, "Value", "Key", rowsMenu);
+            ViewBag.OrderMenu = new SelectList(orderMenuList, "Value", "Key", orderMenu);
+            ViewBag.CurrentPage = currentPage;
+            ViewBag.CurrentColumn = currentColumn;
+            ViewBag.ProductCategories = new SelectList(categories, "id", "name", productCategories);
+            ViewBag.RowsCriteria = rowsCriteria;
+            ViewBag.ShowNextPage = !hideNextPage;
+            ViewBag.ShowNextCol = !hideNextCol;
+     
             Data d = new Data();
-
-            using (SqlConnection con = new SqlConnection("Server=tcp:shoppa135dbserver.database.windows.net,1433;Database=shoppa135db;User ID=shoppa135dbuser@shoppa135dbserver;Password=@C5e1352016;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;"))
-            {
-                using (SqlCommand cmd = new SqlCommand(query, con))
-                {
-                    cmd.CommandType = CommandType.Text;
-
-                    cmd.Parameters.Add("@rows", SqlDbType.VarChar).Value = rows;
-                    cmd.Parameters.Add("@order", SqlDbType.VarChar).Value = order;
-                    cmd.Parameters.Add("@startRow", SqlDbType.Int).Value = startRow;
-                    cmd.Parameters.Add("@startColumn", SqlDbType.Int).Value = startColumn;
-
-                    con.Open();
-
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        List<string> fields = new List<string>();
-                        List<Row> rowsL = new List<Row>();
-
-                        while (reader.Read())
-                        {
-                            if (fields.Count() == 0)
-                            {
-
-                                for (int i = 0; i < reader.FieldCount; i++)
-                                {
-                                    fields.Add(reader.GetName(i));
-                                }
-                            }
-
-                            Row row = new Row();
-                            for (int i = 0; i < reader.FieldCount; i++)
-                            {
-                                row.Values.Add(reader.GetValue(i).ToString());
-                            }
-                            rowsL.Add(row);
-                        }
-
-
-                        d.Fields = fields;
-                        d.Rows = rowsL;
-                    }
-                }
-            }
-            
+            d.PerformQuery(rowsMenu, orderMenu, startRow, startColumn, productCategories);
             return View(d);
         }
 
-        private string query = @"DECLARE @cols            AS NVARCHAR(MAX),
-        @colsWithNoNulls AS NVARCHAR(MAX),
-        @query           AS NVARCHAR(MAX),
-		@colsQueryHelper AS NVARCHAR(MAX),
-		@rowsQueryHelper AS NVARCHAR(MAX)
+        [HttpPost]
+        public ActionResult Sales(FormCollection fc, string rowsMenu = "name", string orderMenu = "alphabet", int productCategories = 0, string button = "Run")
+        {
+            // FILTERING OPTIONS:
+            var rowsMenuList =
+                new List<KeyValuePair<string, string>>()
+                    { new KeyValuePair<string,string>("States", "state"), 
+                      new KeyValuePair<string,string>("Customers", "name")};
 
-SET @rowsQueryHelper =
-	(CASE WHEN @order = 'topk'
-		THEN
-			'SELECT rows, rank FROM (SELECT users.'+@rows+' as rows, RANK() OVER (ORDER BY SUM(orders.price*orders.quantity) DESC) AS rank
-				FROM users LEFT JOIN orders ON orders.user_id = users.id
-				GROUP BY users.'+@rows + ') ranks
-				WHERE rank BETWEEN ' + CAST(@startRow AS VARCHAR(MAX)) +' AND '+ CAST((@startRow+19) AS VARCHAR(MAX))
+            var orderMenuList =
+                new List<KeyValuePair<string, string>>()
+                    { new KeyValuePair<string,string>("Alphabetical", "alphabet"), 
+                      new KeyValuePair<string,string>("Top-K", "topk")};
 
-		ELSE
-			'SELECT rows, rank FROM (SELECT users.'+@rows+' as rows, RANK() OVER (ORDER BY users.'+@rows+') AS rank FROM users) ranks
-				WHERE rank BETWEEN ' + CAST(@startRow AS VARCHAR(MAX)) +' AND '+ CAST((@startRow+19) AS VARCHAR(MAX))
-	END)
+            // Get Categories
+            var categories = context.categories.ToList();
 
-SET @colsQueryHelper =
-	(CASE WHEN @order = 'topk'
-		THEN
-			'SELECT name FROM (
-			SELECT products.name AS name, RANK() OVER (ORDER BY SUM(orders.price*orders.quantity) DESC) AS rank
-			FROM products LEFT JOIN orders ON orders.product_id = products.id
-			GROUP BY products.name) products
-			WHERE rank BETWEEN ' + CAST(@startColumn AS VARCHAR(MAX)) +' AND '+ CAST((@startColumn+9) AS VARCHAR(MAX))
-		ELSE
-			'SELECT name FROM (
-			SELECT products.name AS name, RANK() OVER (ORDER BY name) AS rank
-			FROM products) products
-			WHERE rank BETWEEN ' + CAST(@startColumn AS VARCHAR(MAX)) +' AND '+ CAST((@startColumn+9) AS VARCHAR(MAX))
-	END)
+            string rowsCriteria = rowsMenu == "name" ? "Customers" : "States";
 
-CREATE TABLE #TEMP (name VARCHAR(MAX))
-INSERT INTO #TEMP 
-EXECUTE sp_executesql @colsQueryHelper
+            
+            int currentPage = int.Parse(fc["currentPage"] ?? "0");
+            int currentColumn = int.Parse(fc["currentColumn"] ?? "0");
 
-SET @cols = STUFF((SELECT ',' + QUOTENAME(name) FROM (SELECT name from #TEMP) products FOR XML PATH (''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '' ) 
+            if (button == "Next 20 " + rowsCriteria)
+            {
+                currentPage++;
+            }
+            else if (button == "Next 10 Products")
+            {
+                currentColumn++;
+            }
 
-SET @colsWithNoNulls = STUFF((SELECT ', ISNULL(' + QUOTENAME(name) + ', 0) ' + QUOTENAME(name) FROM (
-			SELECT name FROM #TEMP) products FOR XML PATH (''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '')
+            int startRow = (currentPage * 20) + 1;
+            int startColumn = (currentColumn * 10) + 1;
 
-SET @query = 'SELECT col, ' + @colsWithNoNulls + ' FROM 
-             (
-                 SELECT users.'+@rows+' as col, rank, products.name as name, (orders.price*orders.quantity) as price
-				 FROM users
-				 JOIN 
-				 (
-					'+@rowsQueryHelper+'
-				 ) ranks ON users.'+@rows+' = ranks.rows
-				 LEFT JOIN orders  ON orders.user_id = users.id
-				 LEFT JOIN products ON orders.product_id = products.id
+            bool readOnly = false;
+            bool hideNextPage = false;
+            bool hideNextCol = false;
 
-             ) x
-             PIVOT 
-             (
-                 SUM(price)
-                 FOR name IN (' + @cols + ')
-             ) p'
+            if (currentColumn != 0) 
+            {
+                readOnly = true;
+            }
 
-EXECUTE (@query)
-DROP TABLE #TEMP";
+            int maxCols = 0;
+            if (productCategories != 0)
+            {
+                maxCols = context.products.Count(x => x.category_id == productCategories);
+            }
+            else
+            {
+                maxCols = context.products.Count();
+            }
+
+            if (startColumn + 9 >= maxCols)
+            {
+                hideNextCol = true;
+            }
+            
+            if (currentPage != 0) 
+            {
+                readOnly = true;
+            }
+
+            int maxRows = 0;
+            if (rowsCriteria == "Customers")
+            {
+                maxRows = context.users.Count();
+            }
+            else
+            {
+                maxRows = context.users.Select(x => x.state).Distinct().Count();
+            }
+
+            if (startRow + 19 >= maxRows)
+            {
+                hideNextPage = true;
+            }
+
+            // Select default and assign list
+            ViewBag.RowsMenu = new SelectList(rowsMenuList, "Value", "Key", rowsMenu);
+            ViewBag.OrderMenu = new SelectList(orderMenuList, "Value", "Key", orderMenu);
+            ViewBag.CurrentPage = currentPage;
+            ViewBag.CurrentColumn = currentColumn;
+            ViewBag.ProductCategories = new SelectList(categories, "id", "name", productCategories);
+            ViewBag.RowsCriteria = rowsCriteria;
+            ViewBag.ReadOnly = readOnly;
+            ViewBag.ShowNextPage = !hideNextPage;
+            ViewBag.ShowNextCol = !hideNextCol;
+
+            Data d = new Data();
+            d.PerformQuery(rowsMenu, orderMenu, startRow, startColumn, productCategories);
+            return View(d);
+        }
     }
 }
--- Shoppa/Shoppa/Models/Data.cs
+++ Shoppa/Shoppa/Models/Data.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,124 @@ namespace Shoppa.Models
 {
     public class Data
     {
+        #region Query
+        private static string query = @"DECLARE @cols            AS NVARCHAR(MAX),
+        @colsWithNoNulls AS NVARCHAR(MAX),
+        @query           AS NVARCHAR(MAX),
+		@colsQueryHelper AS NVARCHAR(MAX),
+		@rowsQueryHelper AS NVARCHAR(MAX),
+		@categoryQuery AS NVARCHAR(MAX),
+		@categoryQueryWHere AS NVARCHAR(MAX)
+
+SET @categoryQueryWhere = CASE WHEN @category_id = 0 THEN '' ELSE ' WHERE products.category_id = '+ CAST(@category_id AS VARCHAR(MAX)) END
+SET @categoryQuery = CASE WHEN @category_id = 0 THEN '' ELSE 'LEFT JOIN ( SELECT * FROM products '+@categoryQueryWhere+') products ON orders.product_id = products.id' END
+
+SET @rowsQueryHelper =
+	(CASE WHEN @order = 'topk'
+		THEN
+			'SELECT rows, rank, sum  FROM (SELECT users.'+@rows+' as rows, SUM(orders.price*orders.quantity) AS sum, RANK() OVER (ORDER BY SUM(orders.price*orders.quantity) DESC) AS rank
+				FROM users LEFT JOIN orders ON orders.user_id = users.id '+@categoryQuery+'
+				GROUP BY users.'+@rows + ') ranks
+					WHERE rank BETWEEN ' + CAST(@startRow AS VARCHAR(MAX)) +' AND '+ CAST((@startRow+19) AS VARCHAR(MAX))
+				
+
+		ELSE
+			'SELECT rows, rank, sum FROM (SELECT users.'+@rows+' as rows, SUM(orders.price*orders.quantity) AS sum, RANK() OVER (ORDER BY users.'+@rows+') AS rank FROM users
+			LEFT JOIN orders ON orders.user_id = users.id '+@categoryQuery+'
+				GROUP BY users.'+@rows + '
+			
+			) ranks
+				WHERE rank BETWEEN ' + CAST(@startRow AS VARCHAR(MAX)) +' AND '+ CAST((@startRow+19) AS VARCHAR(MAX))
+				
+	END)
+
+SET @colsQueryHelper =
+	(CASE WHEN @order = 'topk'
+		THEN
+			'SELECT CONCAT(name, '' ('', ISNULL(sum,0), '')''), ISNULL(sum,0), products.id FROM (
+			SELECT products.name AS name, SUM(orders.price*orders.quantity) AS sum, products.id, RANK() OVER (ORDER BY SUM(orders.price*orders.quantity) DESC) AS rank
+			FROM  (SELECT * FROM products '+@categoryQueryWhere+') products LEFT JOIN orders ON orders.product_id = products.id   
+			GROUP BY products.name, products.id) products WHERE rank BETWEEN ' + CAST(@startColumn AS VARCHAR(MAX)) +' AND '+ CAST((@startColumn+9) AS VARCHAR(MAX))
+		ELSE
+			'SELECT  CONCAT(name, '' ('', ISNULL(sum,0), '')''), ISNULL(sum,0), products.id FROM (
+			SELECT products.name AS name, SUM(orders.price*orders.quantity) AS sum, products.id, RANK() OVER (ORDER BY name) AS rank
+			FROM (SELECT * FROM products '+@categoryQueryWhere+')  products LEFT JOIN orders ON orders.product_id = products.id GROUP BY products.name, products.id) products  WHERE rank BETWEEN ' + CAST(@startColumn AS VARCHAR(MAX)) +' AND '+ CAST((@startColumn+9) AS VARCHAR(MAX))
+	END)
+
+CREATE TABLE #TEMP (name VARCHAR(MAX), price INT, id INT)
+INSERT INTO #TEMP 
+EXECUTE sp_executesql @colsQueryHelper
+
+SET @cols = STUFF((SELECT ',' + QUOTENAME(name) FROM (SELECT name from #TEMP) products FOR XML PATH ('')), 1, 1, '' )
+
+SET @colsWithNoNulls = STUFF((SELECT ', ISNULL(' + QUOTENAME(name) + ', 0) ' + QUOTENAME(name) FROM (
+			SELECT name FROM #TEMP) products FOR XML PATH ('')), 1, 1, '')
+
+SET @query = 'SELECT col, ' + @colsWithNoNulls + ' FROM 
+             (
+                 SELECT CONCAT(users.'+@rows+', '' ('', ISNULL(ranks.sum,0), '')'') AS col, rank, #TEMP.name AS name, (orders.price*orders.quantity) as price
+				 FROM users
+				 JOIN 
+				 (
+					'+@rowsQueryHelper+'
+				 ) ranks ON users.'+@rows+' = ranks.rows
+				 LEFT JOIN orders  ON orders.user_id = users.id
+				 LEFT JOIN #TEMP ON orders.product_id = #TEMP.id
+             ) x
+             PIVOT 
+             (
+                 SUM(price)
+                 FOR name IN (' + @cols + ')
+             ) p'
+
+EXECUTE (@query)
+DROP TABLE #TEMP";
+        #endregion
+
+        public void PerformQuery(string rowsMenu, string orderMenu, int startRow, int startColumn, int productCategory)
+        {
+            using (SqlConnection con = new SqlConnection("Server=tcp:shoppa135dbserver.database.windows.net,1433;Database=shoppa135db;User ID=shoppa135dbuser@shoppa135dbserver;Password=@C5e1352016;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;"))
+            {
+                using (SqlCommand cmd = new SqlCommand(Data.query, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.Parameters.Add("@rows", SqlDbType.VarChar).Value = rowsMenu;
+                    cmd.Parameters.Add("@order", SqlDbType.VarChar).Value = orderMenu;
+                    cmd.Parameters.Add("@startRow", SqlDbType.Int).Value = startRow;
+                    cmd.Parameters.Add("@startColumn", SqlDbType.Int).Value = startColumn;
+                    cmd.Parameters.Add("@category_id", SqlDbType.Int).Value = productCategory;
+
+                    con.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        this.Fields = new List<string>();
+                        this.Rows = new List<Row>();
+
+                        while (reader.Read())
+                        {
+                            if (Fields.Count() == 0)
+                            {
+
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Fields.Add(reader.GetName(i));
+                                }
+                            }
+
+                            Row row = new Row();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                row.Values.Add(reader.GetValue(i).ToString());
+                            }
+                            Rows.Add(row);
+                        }
+                    }
+                }
+            }
+        } 
+
         public List<string> Fields { get; set; }
         public List<Row> Rows { get; set; }
     }
